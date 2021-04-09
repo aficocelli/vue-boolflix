b17f44f34517756603b9b310e13d0dde2e3330c5
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-04-09 14:11:00 +0200

    add. hover effect

--- js/script.js
+++ js/script.js
@@ -3,8 +3,20 @@ var app = new Vue ({
   el: '#root',
 
   data:{
-    
+    hover: false,
+
+  },
+
+  methods:{
+    mouseOver: function() {
+      return this.hover = true;
+    },
+    eventTest: function(){
+
+      alert("ciao")
+    }
   }
+
 
 
 });
