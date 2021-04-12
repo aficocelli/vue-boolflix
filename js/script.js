581dc2119bab8a1474b58656c78822a3f49ba040
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-04-12 16:45:25 +0200

    fix. search

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ var app = new Vue ({
     movies: [],
     movieInput: "",
     indexOf: null,
-  
+    apiKey: '5c71d6d9a1c9e5778b11fdc5540b3ed2',
+    urlAxios: 'https://api.themoviedb.org/3/search/',
+
   },
 
   methods: {
@@ -22,11 +24,32 @@ var app = new Vue ({
 
       let myThis = this;
 
-      axios.get('https://api.themoviedb.org/3/search/multi', {
+      axios.get(myThis.urlAxios + 'movie', {
 
         params: {
 
-          api_key: '5c71d6d9a1c9e5778b11fdc5540b3ed2',
+          api_key: myThis.apiKey,
+          query: myThis.movieInput,
+          language: 'it-IT'
+
+        }
+
+      })
+      .then(function(response){
+
+
+
+        myThis.movies = response.data.results;
+
+
+
+      });
+
+      axios.get(myThis.urlAxios + 'tv', {
+
+        params: {
+
+          api_key: myThis.apiKey,
           query: myThis.movieInput,
           language: 'it-IT'
 
